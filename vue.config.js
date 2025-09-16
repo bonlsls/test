a6551ece8a1b21749232a590fba252b56dd98151
Bao Khanh <155916548+bonlsls@users.noreply.github.com>
2025-09-16 22:17:02 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
 
-module.exports = {
+// module.exports = {
+//   devServer: {
+//     port: 3000
+//   },
+// };
+
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
   devServer: {
     port: 3000
   },
-};
+   publicPath: process.env.NODE_ENV === "production"
+    ? "/test/"
+    : "/"
+})
